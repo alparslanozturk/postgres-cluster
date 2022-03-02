7bef383d0fe2e8cc84a4002f0d24d250c70395ca
aozturk <alparslan.ozturk@assistt.com.tr>
2022-03-02 16:23:17 +0300

    pgbackrest ssh sorunu düzeltildi.

--- rst.sh
+++ rst.sh
@@ -22,8 +22,8 @@ echo 'postgres:parola'|chpasswd
 echo 'postgres ALL=(ALL:ALL) NOPASSWD:ALL' > /etc/sudoers.d/postgres
 sed -i '2 i sudo /etc/init.d/ssh start' /usr/local/bin/docker-entrypoint.sh
 "
-docker cp ssh/id_postgres pgbackrest:/var/lib/postgresql/.ssh/
-docker cp ssh/id_postgres.pub pgbackrest:/var/lib/postgresql/.ssh/
+docker cp ssh/id_rsa pgbackrest:/var/lib/postgresql/.ssh/
+docker cp ssh/id_rsa.pub pgbackrest:/var/lib/postgresql/.ssh/
 docker cp ssh/authorized_keys pgbackrest:/var/lib/postgresql/.ssh/
 docker exec -it pgbackrest bash -c " chown -R postgres: /var/lib/postgresql/.ssh "
 docker exec -it pgbackrest bash -c " chmod 600 /var/lib/postgresql/.ssh/* "
